b47e275a46ccf0cc7966b06f05b29e3bde3f972c
Jenny Dcruz <jendcruz23@gmail.com>
2020-05-17 16:32:00 +0530

    api key update

--- nasa-apod1/src/components/NasaPhoto.js
+++ nasa-apod1/src/components/NasaPhoto.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import NavBar from "./NavBar";
 
-const apiKey = process.env.REACT_APP_NASA_KEY;
-
 export default function NasaPhoto() {
     const [photoData, setPhotoData] = useState(null);
 
@@ -11,8 +9,8 @@ export default function NasaPhoto() {
         fetchPhoto();
 
         async function fetchPhoto() {
-            const res = await fetch (
-                'https://api.nasa.gov/planetary/apod?api_key=EEN6Wbe9O99yQUA10N9jJJGrdyXGHw1jjskuGJRY'           
+            // eslint-disable-next-line 
+            const res = await fetch ('https://api.nasa.gov/planetary/apod?api_key=EEN6Wbe9O99yQUA10N9jJJGrdyXGHw1jjskuGJRY'           
             );
             const data = await res.json();
             setPhotoData(data);
